f44f7c214524e805c3d42231d0c429436a0fcad3
NSBPittman <nicksbpittman@gmail.com>
2016-03-31 23:23:03 -0400

    working login

--- app.js
+++ app.js
@@ -2,29 +2,34 @@ var appModule = angular.module('myApp', []);
 
 appModule.controller('MainCtrl', ['mainService','$scope','$http',
         function(mainService, $scope, $http) {
-            $scope.greeting = 'Welcome to BookBuds';
-            $scope.tagline = 'Books are better with Buds'
+            $scope.greeting = 'BookBuddaroos';
             $scope.token = null;
             $scope.error = null;
+//            $scope.roleUser = false;
+//            $scope.roleAdmin = false;
+//            $scope.roleFoo = false;
 
             $scope.login = function() {
                 $scope.error = null;
-                mainService.login($scope.username, $scope.password, $scope.question, $scope.answer).then(function(token) {
+                mainService.login($scope.userName, $scope.password).then(function(token) {
                     $scope.token = token;
                     $http.defaults.headers.common.Authorization = 'Bearer ' + token;
+                    //$scope.checkRoles();
                 },
                 function(error){
                     $scope.error = error
-                    $scope.username = '';
-                    $scope.password = '';
-                    $scope.question = '';
-                    $scope.answer = '';
+                    $scope.userName = '';
                 });
             }
 
+            $scope.checkRoles = function() {
+                mainService.hasRole('user').then(function(user) {$scope.roleUser = user});
+                mainService.hasRole('admin').then(function(admin) {$scope.roleAdmin = admin});
+                mainService.hasRole('foo').then(function(foo) {$scope.roleFoo = foo});
+            }
+
             $scope.logout = function() {
-                $scope.username = '';
-                $scope.password = '';
+                $scope.userName = '';
                 $scope.token = null;
                 $http.defaults.headers.common.Authorization = '';
             }
@@ -39,11 +44,30 @@ appModule.controller('MainCtrl', ['mainService','$scope','$http',
 appModule.service('mainService', function($http) {
     return {
         login : function(username, password) {
-            var data = 'name='+$scope.
-            return $http.post('localhost:8080/user', ).then(function(response) {
-                console.log(response.data)
+            var data = "name="+username+"&password="+password;
+
+            //var data = 'name='+username+'&password=ugh';
+            
+            //var data = "name="+username"&password="password;
+            console.log(data)
+            $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
+
+            return $http.post('http://localhost:8080/users/login', data ).then(function(response) {
+                return response.data.token;
+            });
+            
+            return $http.post('http://localhost:8080/users/login', data).then(function(response){
+                $scope.myWelcome = response;
+                return response.data.token;
+            })
+        },
+
+        hasRole : function(role) {
+            return $http.get('/api/role/' + role).then(function(response){
+                console.log(response);
                 return response.data;
             });
-        },
+        }
     };
 });
